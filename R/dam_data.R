c467f4b1d7627c774eaa222276440cc7e48edc3c
Stephen Holsenbeck <stephen@virgalabs.io>
2022-02-02 14:47:53 -0500

    Add `net_check` to stop if no internet
     - `update_dam_options`: meant to be run everyone once in a while to update the BOR dam & link list
     - `dam_select`: returns all selectable dams if no `name` supplied, otherwise finds the closest match. Prompts if multiple matches and interactive.
     - `dam_tables`: pulls detail data efficiently for a given dam

--- R/dam_data.R
+++ R/dam_data.R
@@ -1,30 +1,39 @@
 
 .base_url <- "https://www.usbr.gov/projects/index.php"
-index_htm <- function() {
+net_check <- function(abort = TRUE) {
   if (!curl::has_internet())
-    rlang::abort("This function needs an active internet connection to function.")
-  xml2::read_html(.base_url)
+    rlang::exec(purrr::when(abort, . ~ rlang::abort, ~ rlang::warn), "This function needs an active internet connection to function.")
 }
 
 
-#' All available dams for lookup
-#' @return \code{(chr)} of all dam names
+update_dam_options <- function() {
+  net_check()
+  .dam_opts <- rvest::html_elements(xml2::read_html(.base_url), xpath = '//option[contains(text(), "Select a dam")]/following-sibling::option') |>
+  {\(x) {rlang::set_names(rvest::html_attr(x, "value"), stringr::str_replace(rvest::html_text(x), "\\`", "'"))}}()
+  dump(".dam_opts", "R/dam_opts.R")
+}
 
-dam_options <- function() rvest::html_elements(index_htm(), xpath = '//option[contains(text(), "Select a dam")]/following-sibling::option') |> rvest::html_text()
 
-dam_select <- function(dam) {
-  out <- dam_options()[agrep(dam, dam_options(), ignore.case = TRUE)]
-  if (length(out) > 1 && interactive()) {
-    out <- menu(out)
-  } else if (length(out) > 1) {
-    rlang::warn(paste0("Multiple matches:\n",paste0(out, collapse = "\n"), "\nSelected ", out[1]))
-    out <- out[1]
-  }
+#' Fuzzy search for a dam name or view all
+#' @param name \code{(chr)} of the dam. **Optional** leave blank to return all options
+#' @return \code{(named chr)} with the url named according to the dam
+dam_select <- function(name) {
+  if (!missing(name)) {
+    out <- .dam_opts[agrep(name, names(.dam_opts), ignore.case = TRUE)]
+    if (length(out) > 1 && interactive()) {
+      out <- out[menu(names(out))]
+    } else if (length(out) > 1) {
+      rlang::warn(paste0("Multiple matches:\n",paste0(names(out), collapse = "\n"), "\nSelected ", names(out)[1]))
+      out <- out[1]
+    }
+    out
+  } else
+    out <- .dam_opts
   out
 }
 
 #' Get details on a dam
-#' @description See `dam_options` for available dams
+#' @description See `dam_select` for available dams
 #' @param dam \code{(chr)} Name of the dam to lookup
 #'
 #' @return \code{(list)} of tables
@@ -38,25 +47,15 @@ dam_select <- function(dam) {
 #' @examples
 #' dam_tables("Blue Mesa")
 dam_tables <- function(dam) {
-  if (missing(dam))
-    .txt <- "Select a dam"
-  else
-    .txt <- dam_select(dam)
-  href <- rvest::html_element(index_htm(), xpath = glue::glue('//option[contains(text(), "{.txt}")]')) |>
-    rvest::html_attr("value")
-  if (curl::has_internet()) {
-    .h <- xml2::read_html(httr::modify_url(.base_url, path = href))
-    out <- purrr::map(rlang::set_names(c("General", "Dimensions", "Hydraulics & Hydrology")), ~{
-      el <- rvest::html_elements(.h, xpath = glue::glue("//h4[contains(text(), '{.x}')]/following-sibling::table[1]"))
-      if (!is.na(el))
-        out <- rvest::html_table(el)[[1]]
-      else
-        out <- NULL
-
-    }) |> purrr::compact()
-  } else {
-    out <- NULL
-    rlang::warn(glue::glue("No internet. Cannot fetch tables for {dam}"))
-  }
+  net_check()
+  .path <- dam_select(dam)
+  .h <- xml2::read_html(httr::modify_url(.base_url, path = .path))
+  out <- purrr::map(rlang::set_names(c("General", "Dimensions", "Hydraulics & Hydrology")), ~{
+    el <- rvest::html_elements(.h, xpath = glue::glue("//h4[contains(text(), '{.x}')]/following-sibling::table[1]"))
+    if (UU::is_legit(el))
+      out <- rvest::html_table(el)[[1]]
+    else
+      out <- NULL
+  }) |> purrr::compact()
   out
 }

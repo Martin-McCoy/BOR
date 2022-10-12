8bc84370a1f0bae64dc078ba5c32a95ddb2c3bee
Stephen Holsenbeck <stephen@virgalabs.io>
2022-10-12 11:31:39 -0400

    Adopt shiny::sliderInput argument names

--- R/mead_powell_defaults.R
+++ R/mead_powell_defaults.R
@@ -1,7 +1,7 @@
 #' Default Elevations for Mead/Powell for use in sliders
 #' @export
 mead_powell_slider_defaults <-
-structure(list(cat = c("Powell Pool Elevation", "Mead Pool Elevation"
-), min = c(3400, 975), max = c(3600, 1100), default = c(3500,
+structure(list(label = c("Powell Pool Elevation", "Mead Pool Elevation"
+), min = c(3400, 975), max = c(3600, 1100), value = c(3500,
 1025), step = c(25, 25)), class = "data.frame", row.names = c(NA,
 -2L))

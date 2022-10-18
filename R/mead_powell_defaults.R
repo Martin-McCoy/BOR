1832029782fd3dcebf073a13734d1dc309e18fd9
Stephen Holsenbeck <stephen@virgalabs.io>
2022-10-18 15:33:26 -0400

    Update mead/powell defaults

--- R/mead_powell_defaults.R
+++ R/mead_powell_defaults.R
@@ -2,6 +2,6 @@
 #' @export
 mead_powell_slider_defaults <-
 structure(list(label = c("Powell Pool Elevation", "Mead Pool Elevation"
-), min = c(3400, 975), max = c(3600, 1100), value = c(3500,
-1025), step = c(25, 25)), class = "data.frame", row.names = c(NA,
+), min = c(3400, 975), max = c(3600, 1100), value = c(3525,
+1020), step = c(5, 5)), class = "data.frame", row.names = c(NA,
 -2L))

0c7f6746184e7b0cb278de7e3cdc5837d124d262
Stephen Holsenbeck <stephen@virgalabs.io>
2022-12-22 14:31:58 -0500

    Add `powell_mead_elevations`

---
+++ R/obj_powell_mead_elevations.R
@@ -0,0 +1,16 @@
+#' @export
+powell_mead_elevations <-
+structure(list(Reservoir = c("Powell", "Powell", "Powell", "Powell", 
+"Powell", "Powell", "Mead", "Mead", "Mead", "Mead", "Mead", "Mead", 
+"Mead", "Mead"), `Elevation Description` = c("Full Pool", "Upper Elevation Balancing Tier", 
+"Mid Elevation Release Tier", "Lower Elevation Balancing Tier", 
+"Minimum Power Pool", "Dead Pool", "Full Pool", "Normal Condition", 
+"Tier 1 Shortage Condition", "Tier 2 Shortage Condition", "Tier 3 Shortage Condition", 
+"Minimum Power Pool", "Dead Pool", "1,090"), Operator = c(NA, 
+NA, NA, "<", NA, NA, NA, ">=", "<=", "<=", "<=", NA, NA, NA), 
+    `Elevation (ft)` = c(3700, 3575, 3525, 3525, 3490, 3370, 
+    1229, 1090, 1075, 1050, 1025, 950, 895, 1090), `Current RiverViz Elevs.` = c(NA, 
+    NA, "X", NA, "X", NA, NA, "X", "X", "X", NA, NA, NA, "X: Unknown, 1000’"
+    ), `Proposed RiverViz Elevs.` = c(NA, "X", "X", NA, "X", 
+    "X", NA, "X", "X", "X", "X", "X", "X", NA)), class = c("tbl_df", 
+"tbl", "data.frame"), row.names = c(NA, -14L))
--- R/utils_bor.R
+++ R/utils_bor.R
@@ -12,3 +12,27 @@
 bor_nm2chr <- function(x) {
   trimws(tolower(stringr::str_extract(x, stringr::regex(UU::regex_or(c("mead", "powell", "navajo", "fontenelle", "havasu", "mohave", "blue mesa", "flaming gorge")), ignore_case = TRUE))))
 }
+
+#' Update `powell_mead_elevations` table
+#' @description A table of key elevations pertaining to Lake's Mead & Powell
+#' @return \code{file}
+
+
+update_powell_mead_elevations <- function() {
+  virgaUtils::google_auth()
+  powell_mead_elevations <- googlesheets4::read_sheet(
+    "https://docs.google.com/spreadsheets/d/1lRtySzf-BPS-mUxjTPYX4Aky9x0yiJOJt_AHpgVpins/edit#gid=1728077509",
+    sheet = "Powell & Mead thresholds",
+    col_types = glue::glue_collapse(c(
+      Reservoir = "c",
+      `Elevation Description` = "c",
+      Operator = "c",
+      `Elevation (ft)` = "n",
+      `Current RiverViz Elevs.` = "c",
+      `Proposed RiverViz Elevs.` = "c"
+    )))
+  file <- "R/obj_powell_mead_elevations.R"
+  UU::mkpath(file, mkfile = TRUE)
+  write("#' @export", file)
+  dump("powell_mead_elevations", file, append = TRUE)
+}
